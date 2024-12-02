fedb085d21b668ae69a5410a6b481c10874b3d51
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2024-12-02 12:59:39 +0400

    clean-up app component

---
+++ src/config/CalendarContent.ts
@@ -0,0 +1,84 @@
+import { ContentType } from "types/contentTypes";
+
+export const CalendarContent = [
+  {
+    name: 'bell',
+    src: '/images/bell.png',
+    color: '#ede6dd',
+    type: ContentType.VIDEO,
+    content: 'https://www.youtube.com/embed/_U9sEax4AoM'
+  },
+  {
+    name: 'candy-cane',
+    src: '/images/candy-cane.png',
+    color: '#2b2b2b',
+    type: ContentType.TEXT,
+    content: 'CHALLENGE - LOVE',
+    ctaText: "Discover",
+    ctaTarget: "https://pricey-carob-c0f.notion.site/Love-1-John-4-7-11-1509855408a38038a7a2e0155b965482"
+  },
+  {
+    name: 'chimney',
+    src: '/images/chimney.png',
+    color: '#ed9474',
+    type: ContentType.TEXT,
+    content: '🛒✨🔥🎄'
+  },
+  {
+    name: 'christmas-candy',
+    src: '/images/christmas-candy.png',
+    color: '#97c9c4',
+    type: ContentType.TEXT,
+    content: 'CHALLENGE - JOY',
+    ctaText: "Discover",
+    ctaTarget: "https://pricey-carob-c0f.notion.site/Joy-Psalms-63-4-7-1509855408a380ee9107fac6993bde12?pvs=4"
+  },
+  { name: 'chimney', src: '/images/chimney.png', color: '#ede6dd',      type: ContentType.TEXT,
+    content: 'CHALLENGE - PEACE',
+    ctaText: "Discover",
+    ctaTarget: "https://pricey-carob-c0f.notion.site/Peace-Philippians-4-6-7-1509855408a38033b354f71fd367509c?pvs=4"
+},
+  {
+    name: 'christmas-decor',
+    src: '/images/christmas-decor.png',
+    color: '#ad251e'
+  },
+  {
+    name: 'christmas-gift',
+    src: '/images/christmas-gift.png',
+    color: '#97c9c4'
+  },
+  {
+    name: 'christmas-tree',
+    src: '/images/christmas-tree.png',
+    color: '#194a60'
+  },
+  { name: 'crystal-ball', src: '/images/crystal-ball.png', color: '#ede6dd' },
+  {
+    name: 'decoration-lamp',
+    src: '/images/decoration-lamp.png',
+    color: '#2b2b2b'
+  },
+  {
+    name: 'decoration-lamps',
+    src: '/images/decoration-lamps.png',
+    color: '#ede6dd'
+  },
+  { name: 'fireplace', src: '/images/fireplace.png', color: '#194a60' },
+  {
+    name: 'christmas-gift',
+    src: '/images/christmas-gift.png',
+    color: '#ad251e'
+  },
+  { name: 'gift-sledge', src: '/images/gift-sledge.png', color: '#97c9c4' },
+  { name: 'santa-clous', src: '/images/santa-clous.png', color: '#ede6dd' },
+  { name: 'sledge', src: '/images/sledge.png', color: '#2b2b2b' },
+  { name: 'snowflake', src: '/images/snowflake.png', color: '#ed9474' },
+  { name: 'santa-clous', src: '/images/santa-clous.png', color: '#ede6dd' },
+  { name: 'snowman', src: '/images/snowman.png', color: '#2b2b2b' },
+  { name: 'winter-glove', src: '/images/winter-glove.png', color: '#ad251e' },
+  { name: 'winter-glove', src: '/images/winter-glove.png', color: '#97c9c4' },
+  { name: 'winter-hat', src: '/images/winter-hat.png', color: '#ede6dd' },
+  { name: 'winter-sock', src: '/images/winter-sock.png', color: '#ad251e' },
+  { name: 'wreath', src: '/images/wreath.png', color: '#97c9c4' }
+]
